fd78854369f3f296777560219f13f518de0f6ebd
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-12 17:04:45 +0900

    2023.12

---
+++ foobar-admin-client2/src/main/kotlin/gogo/demo/boot/admin_client2/AdminClient2Application.kt
@@ -0,0 +1,11 @@
+package gogo.demo.boot.admin_client2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdminClient2Application
+
+fun main(args: Array<String>){
+    runApplication<AdminClient2Application>(*args)
+}
---
+++ foobar-admin-client2/src/test/kotlin/gogo/demo/boot/admin_client2/AdminClient2ApplicationTest.kt
@@ -0,0 +1,14 @@
+package gogo.demo.boot.admin_client2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdminClient2ApplicationTest {
+
+    @Test
+    fun contextLoads(){
+
+    }
+
+}
